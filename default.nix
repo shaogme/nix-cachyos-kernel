c90c6ea18a7edf901a8d04240a59ff8f6602e614
韶光 <87350612+ShaoG-R@users.noreply.github.com>
2026-01-16 18:12:51 +0800

    Refactor Nix configuration to streamline kernel package handling by combining kernel definitions and removing unnecessary attributes.

--- default.nix
+++ default.nix
@@ -4,7 +4,8 @@
 let
   inherit (pkgs) lib;
   callPackage = lib.callPackageWith (pkgs // { inherit inputs; });
+
+  kernels = callPackage ./kernel-cachyos/default.nix { };
+  kernelPackages = callPackage ./kernel-cachyos/packages.nix { };
 in
-lib.removeAttrs
-  (callPackage ./kernel-cachyos/packages.nix { })
-  [ "override" "overrideDerivation" ]
+kernels // (lib.removeAttrs kernelPackages [ "override" "overrideDerivation" ])
